6df33beba202d58682e500f2981eb09d20976b3d
Imran Hossen <imranbappy.official@gmail.com>
2021-01-30 17:06:07 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import Countries from './components/Countries/Countries';
+import Cart from './components/Cart/Cart';
 
 function App() {
+
+  const [countries, setCountries] = useState([])
+
+  useEffect(() => {
+    fetch('https://restcountries.eu/rest/v2/all')
+      .then(response => response.json())
+      .then(data => {
+        setCountries(data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+  const [search, setSearch] = useState()
+  console.log(search);
+  const searchHandler = e => {
+    let result = countries.find(country => country.name.toLowerCase() === e.target.value.toLowerCase())
+    setSearch(result)
+  }
+
+  const [cart, setCart] = useState([])
+
+  const handelAddCountry = (cuntray) => {
+    let newCountry = [...cart, cuntray]
+    setCart(newCountry)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>ALl countries: {countries.length}</h1>
+        <p>{cart.length}</p>
+        <Cart cart={cart} />
+        <input placeholder="Search by cuntray name.." className="searchIn" onChange={searchHandler} type="text" />
+        {
+          search ? <p>Name:
+            {search.name}&nbsp;|
+            alpha2Code : {search.alpha2Code} &nbsp;|
+              area: {search.area}&nbsp;|
+             demonym: {search.demonym}&nbsp;|
+             region: {search.region}&nbsp;|
+             capital: {search.capital}&nbsp;
+          </p> : ''
+        }
+        <ol>
+          {
+            countries.map(country => <Countries
+              key={country.alpha2Code}
+              handelAddCountry={handelAddCountry}
+              country={country}
+            />)
+          }
+        </ol>
       </header>
     </div>
   );
---
+++ src/components/Countries/Countries.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './countries.css'
+const Countries = (props) => {
+    const {
+        name,
+        population,
+        subregion,
+        topLevelDomain,
+        alpha2Code, area,
+        demonym, region,
+        capital,
+        flag,
+        timezones: [timeZone]
+    }
+        = props.country
+    let style = {
+
+    }
+
+
+
+    return (
+        <div style={{ border: '1px solid', marginTop: 20, width: 350 }}>
+            <img className='img' src={flag} alt="" />
+            <h4 >Name: {name}</h4>
+            <h4 className='body'>alpha2Code: {alpha2Code}</h4>
+            <p className='body'>region: {region}</p>
+            <button className='button' onClick={() => props.handelAddCountry(props.country)}>Add Country</button>
+        </div >
+    );
+};
+
+export default Countries;
